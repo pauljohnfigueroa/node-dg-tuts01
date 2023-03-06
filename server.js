f8dfdbaef3310c4515be33090b3a5407fa69aa3a
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-06 16:30:14 +0800

    NodeJS - cors, middlewares

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const cors = require('cors')
 const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
 
 // middlewares
@@ -10,10 +11,11 @@ const PORT = process.env.PORT || 3500
 app.use(logger)
 // Cross Origin Resource Sharing
 // you must have a shitelist of allowed domains
-const whitelist = ['http://localhost:3000']
+const whitelist = ['https://www.google.com', 'http://localhost:3500/']
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+  origin: (origin, callback) => {
+    // remove || !origin in production
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
@@ -43,8 +45,18 @@ app.get('/old-page(.html)?', (req, res) => {
   res.redirect(301, 'new-page.html')
 })
 
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+// default route
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 file not found.' })
+  } else {
+    res.type('txt').send('404 file not found.')
+  }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
